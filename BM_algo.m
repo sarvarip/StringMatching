6be1f285d1df99f23b3b872f3494c7e523d2212d
Peter Sarvari <sarvarip@gmail.com>
2019-03-19 23:41:53 -0700

    Add files via upload

--- BM_algo.m
+++ BM_algo.m
@@ -3,6 +3,7 @@ function [occurences] = BM_algo(pattern, text) %#codegen
 % k is the pointer that is "shifted"
 % occurences denotes where the exact matching ENDS in the text
 % example: BM_algo('AGCAGTACGTAGCCTAGCAGTA', 'ACGTAAGGCAGTACCAGCAGTACGTAGCCTAGCAGTAACGGTGCTGTAGCAGTACGTAGCCTAGCAGTAAGTACCAGCTA')
+% example 2: BM_algo('ACTGAACTGACT','CCGCTGAACTGACTGACTGAACTGACTTGAACTGACT');
 % this example uses the good suffix rule too!
 
 lookup = BM_bad_char_rule(pattern);
@@ -27,22 +28,24 @@ while k<=m
     else
         mismatching_char = text(h); 
         num = i;
-        if mismatching_char == 'A'
-            lookup_val = lookup(1,num-1);
-        elseif mismatching_char == 'C'
-            lookup_val = lookup(2,num-1);
-        elseif mismatching_char == 'G'
-            lookup_val = lookup(3,num-1);
-        elseif mismatching_char == 'T'
-            lookup_val = lookup(4,num-1);
-        else
-            disp('Error: wrong dictionary is assumed');
-            pause;
-        end
-        if isempty(lookup_val)
+        if num == 1
             lookup_val = 0;
+        else
+            if mismatching_char == 'A'
+                lookup_val = lookup(1,num-1);
+            elseif mismatching_char == 'C'
+                lookup_val = lookup(2,num-1);
+            elseif mismatching_char == 'G'
+                lookup_val = lookup(3,num-1);
+            elseif mismatching_char == 'T'
+                lookup_val = lookup(4,num-1);
+            else
+                disp('Error: wrong dictionary is assumed');
+                pause;
+            end
         end
-        if l_vals(i) > 0 && i<n %i<n meaning that there was at least a match
+        if l_vals(i) > 0 && i<n %i<n meaning that there was at least one 
+            %character matching
             k = k + max(n-l_vals(i), num-lookup_val);
         else
             k = k + num-lookup_val;
--- BM_bad_char_rule.m
+++ BM_bad_char_rule.m
@@ -2,20 +2,13 @@ function [lookup] = BM_bad_char_rule(pattern) %#codegen
  %% Assuming that pattern is DNA, so alphabet is {A,C,G,T}
  
  n = length(pattern);
- lookup = -1*ones(4,n);
+ lookup = zeros(4,n);
  
-if pattern(1) == 'A'
-    lookup(1,1) = 1;
-elseif pattern(2) == 'C'
-    lookup(2,1) = 1;
-elseif pattern(3) == 'G'
-    lookup(3,1) = 1;
-else
-    lookup(4,1) = 1;
-end
  
-for i=2:1:n
-    lookup(:,i) = lookup(:,i-1);
+for i=1:1:n
+    if i > 1
+        lookup(:,i) = lookup(:,i-1);
+    end
     if pattern(i) == 'A'
         lookup(1,i) = i;
     elseif pattern(i) == 'C'
